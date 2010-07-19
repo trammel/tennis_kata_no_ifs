d0c487800088ce3d93d0c12c239118f9695c9471
Jonathon Padfield <jonathon.padfield@rea-group.com>
2010-07-19 13:48:02 +1000

    WIP

--- lib/game.rb
+++ lib/game.rb
@@ -1,88 +1,63 @@
 class Game
-  def initialize
-    @player1_score_pos = 0
-    @player2_score_pos = 0
-    @winner = ''
-    @score
-    @scores = [
-            lambda {|player| @score[player - 1] = 0 },
-            lambda {|player| @score[player - 1] = 15},
-            lambda {|player| @score[player - 1] = 30},
-            lambda {|player| @score[player - 1] = 40},
-            lambda {|player|
-              different = @player1_score_pos - @player2_score_pos
-              codes = {
-                -2 => lambda {
-                  @winner = 'Player 2'
-                },
-                0  => lambda {
-                  @score = 'Deuce'
-                },
-                2  => lambda {
-                  @winner = 'Player 1'
-                }
-              }
 
-              codes.
-    }]
+  def initialize
+    @winner = ''
+    @score = [0, 0]
+    @player1_score = 0
+    @player2_score = 0
+  end
+
+
+  def result player_name, opponent_name, player_score, opponent_score
+    game = {
+            0 => {
+                0  => [15, 0,  [15, 0 ], ''],
+                15 => [15, 15, [15, 15], ''],
+                30 => [15, 30, [15, 30], ''],
+                40 => [15, 40, [15, 40], ''],
+            },
+            15 => {
+                0  => [30, 0,  [30, 0 ], ''],
+                15 => [30, 15, [30, 15], ''],
+                30 => [30, 30, [30, 30], ''],
+                40 => [30, 40, [30, 40], ''],
+            },
+            30 => {
+                0  => [40, 0,  [40,  0], ''],
+                15 => [40, 15, [40, 15], ''],
+                30 => [40, 30, [40, 30], ''],
+                40 => ['Deuce', 'Deuce', 'Deuce', ''],
+           },
+            40 => {
+                0  => [winner, 0 ,  "winner #{player_name}", player_name],
+                15 => [winner, 15,  "winner #{player_name}", player_name],
+                30 => [winner, 30,  "winner #{player_name}", player_name],
+            },
+            'Deuce' => {
+                'Deuce' => ["advantage #{player_name}", "advantage #{player_name}", "advantage #{player_name}"]
+            },
+            "Advantage #{player_name}" => {
+                "Advantage #{player_name}" => ["winner #{player_name}", 'Deuce', player_name]
+            },
+            "Advantage #{opponent_name}" => {
+                "Advantage #{player_name}" => ['Deuce', 'Deuce', 'Deuce'],
+            },
+    }
+    raise "couldnt find #{player_score}" unless game.has_key? player_score
+    return game[player_score][opponent_score]
   end
 
   def player1_score
-    @player1_score_pos += 1
-    @scores[@player1_score_pos].call 1
+    @player1_score, @player2_score, @score, @winner = result('Player 1', 'Player 2', @player1_score, @player2_score)
   end
 
   def player2_score
-    @player2_score_pos += 1
-    @scores[@player2_score_pos].call 2
+    @player2_score, @player1_score, @score, @winner = result('Player 2', 'Player 1', @player2_score, @player1_score)
   end
 
-#  def begining_score
-#      [@scores[@player1_score_pos].call, @scores[@player2_score_pos].call]
-#  end
-#
-#  def score
-#    combo = (@player1_score_pos + @player2_score_pos > 6)
-#    methods = {
-#            true => lambda{begining_score},
-#            false => lambda{end_game_score}
-#            }
-#    method = methods[combo]
-#    method.call
-#  end
-#
-#
-#  def end_game_score
-#    end_scores = {
-#            -1 => 'adv 1',
-#            0 => 'Deuce',
-#            1 => 'adv 2'
-#     }
-#    diff = (@player1_score_pos - @player2_score_pos)
-#    return end_scores[diff]
-#
-#  end
-
-#  def player1_score
-#
-#    increment = lambda { 1 }
-#    score_table = [
-#            #  0            15          30          40
-#            [increment, increment, increment, increment, increment], # 0
-#            [increment, increment, increment, increment, increment], # 0
-#            [increment, increment, increment, increment, increment], # 0
-#            [increment, increment, increment, increment, increment], # 0
-#    ]
-#
-#
-#    @player1_score_pos += 1
-#    @scores[@player1_score_pos].call 1
-#  end
-#
-#  def player2_score
-#    @player2_score_pos += 1
-#    @scores[@player2_score_pos].call 2
-#  end
+  def score
+    @score
+  end
 
   def winner
     @winner

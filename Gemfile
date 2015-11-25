7c6b6c674063fa524b591a7e7e5c34dba43cf270
Jonathon Padfield <jonathon.padfield@gmail.com>
2015-11-25 12:59:55 +0800

    Initial(ish) commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# Use proxied gem dists for performance reasons in EC2 Env
+source 'https://rubygems.org'
+
+gem 'rake'
+
+group :development, :test do
+  gem 'simplecov'
+  gem 'rspec'
+end
+
--- spec/tennis_score_spec.rb
+++ spec/tennis_score_spec.rb
@@ -4,6 +4,6 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'game')
 describe Game do
   it "should start at 0-0" do
     game = Game.new
-    game.score.should == [0,0]
+    expect(game.score).to eq([0,0])
   end
 end

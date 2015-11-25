61cae1a652cef26b99e0e6db68690c4a3e361eb4
Jonathon Padfield <jonathon.padfield@gmail.com>
2015-11-25 11:47:00 +0800

    Added Gemfile, required rspec, simplecov, and have it initialized.

--- spec/tennis_score_spec.rb
+++ spec/tennis_score_spec.rb
@@ -1,9 +1,9 @@
-require 'spec'
-require File.join(File.dirname(__FILE__), '..', 'lib', 'game')
+require 'rspec'
+require_relative File.join('..', 'lib', 'game')
 
 describe Game do
   it "should start at 0-0" do
     game = Game.new
-    game.score.should == [0,0]
+    expect(game.score).to eq([0,0])
   end
 end
